68d6fe092311ea326b47c6726de1eaefd8658b08
Mauro Dorigo <mau.dorigo@outlook.es>
2020-08-24 15:21:27 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,6 +1,11 @@
 def find_max_value(array)
-  counter = 0
-  while counter < array.length do
-    if 
+  count = 0
+  max_value = -1
+  while count < array.length do
+    if max_value < array[count]
+      max_value = array[count]
+    end  
     counter += 1
+  end
+  max_value
 end
